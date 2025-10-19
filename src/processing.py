96aaf1ab321a3034f4a7f6217c443d369ca9c99b
Farkhad_Zainullin <madflyzero@gmail.com>
2025-10-19 16:15:16 +0300

    Добавление readme

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,17 @@
 from typing import List, Dict
 
 
-def filter_by_state(data: List[Dict], state: str = "EXECUTED") -> List[Dict]:
+def filter_by_state(transaction_data: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """
     Функция фильтрует список словарей по значению ключа 'state'.
 
     data: Список словарей для фильтрации.
     state: Значение для ключа 'state' (по умолчанию 'EXECUTED').
     """
-    return [item for item in data if item.get("state") == state]
-def sort_by_date(data: List[Dict], descending: bool = True) -> List[Dict]:
+    return [item for item in transaction_data if item.get("state") == state]
+
+
+def sort_by_date(transaction_data: List[Dict], descending: bool = True) -> List[Dict]:
     """
     Функция сортирует список словарей по дате.
 
@@ -18,4 +20,4 @@ def sort_by_date(data: List[Dict], descending: bool = True) -> List[Dict]:
     """
 
     # Сортировка списка словарей по дате
-    return sorted(data, key=lambda x: x['date'], reverse=descending)
+    return sorted(transaction_data, key=lambda x: x["date"], reverse=descending)
