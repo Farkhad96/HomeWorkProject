fec9d4c88eaea60a3e5e17af3f30bdb28160e5fe
Farkhad_Zainullin <madflyzero@gmail.com>
2025-11-15 22:52:34 +0300

    Добавление логгеров для utils и masks

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,13 @@
 import json
+import logging
 from typing import Any, Dict, List
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("../logs/utils.log", mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s : %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def load_transactions(file_path: str) -> List[Dict[str, Any]]:
@@ -17,13 +25,18 @@ def load_transactions(file_path: str) -> List[Dict[str, Any]]:
     Возвращает:
         Список словарей (транзакций). Если что-то не так — [].
     """
+    logger.info(f"Функция load_transactions приняла на вход {file_path}")
     try:
         with open(file_path, "r", encoding="utf-8") as f:
             data = json.load(f)
+            logger.info(f"Загружен файл мз {file_path} ")
             if not data:
+                logger.info("Файл пустой, возвращается пустой список")
                 return []
             if isinstance(data, list):
+                logger.info("Файл прочитан успешно")
                 return data
             return []
-    except (FileNotFoundError, json.JSONDecodeError):
+    except (FileNotFoundError, json.JSONDecodeError) as ex:
+        logger.info(f"Произошла ошибка {ex}")
         return []
